62675ece105ee30580d19282e65357102dff65e4
Caio <matossousacaio.cms@gmail.com>
2024-01-08 22:10:32 -0300

    repetição da base de dados

--- codigo.py
+++ codigo.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
 import pandas
-pyautogui.PAUSE = 1
+#pyautogui.PAUSE = 0.2
 
 pyautogui.press('win')
 pyautogui.write('chrome')
@@ -29,12 +29,53 @@ pyautogui.write('minha senha')
 #fazer login
 pyautogui.press('tab')
 pyautogui.press('enter')
-
+time.sleep(2)
 #Passo 3 - Importar base de dados
 
 
 tabela = pandas.read_csv('produtos.csv')
+#print(tabela)
 
-
-#Passo 4 - Cadastrar um produto
 #Passo 5 - Repetir até acabar a base de dados
+for linha in tabela.index:
+    #Passo 4 - Cadastrar um produto
+    pyautogui.click(x=576, y=255)
+    #codigo
+
+    codigo = tabela.loc[linha, 'codigo']
+    pyautogui.write(codigo)
+    pyautogui.press('tab')
+    #marca
+
+    pyautogui.write(tabela.loc[linha, 'marca'])
+    pyautogui.press('tab')
+    #tipo
+
+    pyautogui.write(tabela.loc[linha, 'tipo'])
+    pyautogui.press('tab')
+
+    #categoria
+    pyautogui.write(str(tabela.loc[linha, 'categoria']))
+    pyautogui.press('tab')
+
+    #preço
+    pyautogui.write(str(tabela.loc[linha, 'preco_unitario']))
+    pyautogui.press('tab')
+
+    #custo
+
+    pyautogui.write(str(tabela.loc[linha, 'custo']))
+    pyautogui.press('tab')
+
+    #obs
+    obs = tabela.loc[linha, 'obs']
+    if not pandas.isna(obs):
+        pyautogui.write(obs)
+    
+    #enviar o produto
+    pyautogui.press('tab')
+    pyautogui.press('enter')
+
+    pyautogui.scroll(5000)
+
+
